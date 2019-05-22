71ff57593a5ecf58a05c356b848920241c644af6
Kurt Normaal <Kurt.Dhaene@UGent.be>
2019-05-22 14:35:27 +0200

    remove unused code

--- src/Parser/FragmentHandler.js
+++ src/Parser/FragmentHandler.js
@@ -14,47 +14,7 @@ export default class FragmentHandler {
             signalGroups.push(quad.subject.value);
         });
 
-        //overlopen van alle observaties in een fragment, gesorteerd met oudste eerst
-        // await store.getQuads(null, namedNode('http://www.w3.org/ns/prov#generatedAtTime'), null).sort(function (a, b) {
-        //         a = new Date(a.object.value).getTime();
-        //         b = new Date(b.object.value).getTime();
-        //
-        //         return a < b ? -1 : a > b ? 1 : 0;
-        //     }
-        // ).forEach((observation) => {    //TODO: probably only need the last observation
-        //     let generatedAtTime = observation.object.value;
-        //
-        //     signalGroups.forEach((signalGroup) => {
-        //         let signalState = store.getQuads(namedNode(signalGroup), namedNode('https://w3id.org/opentrafficlights#signalState'), null, observation.subject)[0];
-        //         if(signalState) {
-        //             let minEndTime = store.getQuads(signalState.object, namedNode('https://w3id.org/opentrafficlights#minEndTime'), null, observation.subject)[0].object.value;
-        //             let maxEndTime = store.getQuads(signalState.object, namedNode('https://w3id.org/opentrafficlights#maxEndTime'), null, observation.subject)[0].object.value;
-        //             let signalPhase = store.getQuads(signalState.object, namedNode('https://w3id.org/opentrafficlights#signalPhase'), null, observation.subject)[0].object.value;
-        //             let likelyTime = undefined;
-        //             try{
-        //                 likelyTime = store.getQuads(signalState.object, namedNode('https://w3id.org/opentrafficlights#likelyTime'), null, observation.subject)[0].object.value;
-        //             }
-        //             catch (e) {
-        //                 //TODO: clean!
-        //                 console.log("no likelyTime");
-        //             }
-        //
-        //             if(!returnObject[signalGroup]){
-        //                 returnObject[signalGroup] = {};
-        //             }
-        //             returnObject[signalGroup] = {
-        //                 "generatedAtTime": generatedAtTime,
-        //                 "minEndTime": minEndTime,
-        //                 "maxEndTime": maxEndTime,
-        //                 "signalPhase": signalPhase,
-        //                 "likelyTime": likelyTime
-        //             }
-        //
-        //         }
-        //     });
-        //
-        // });
-
+        //only use most recent observation
         let observation = await store.getQuads(null, namedNode('http://www.w3.org/ns/prov#generatedAtTime'), null).sort(function (a, b) {
                 a = new Date(a.object.value).getTime();
                 b = new Date(b.object.value).getTime();
