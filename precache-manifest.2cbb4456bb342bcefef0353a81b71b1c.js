1d56f7549654e1df3cb126aaf3a4fc2d8cf915f9
Kurt Normaal <Kurt.Dhaene@UGent.be>
2019-06-10 00:22:57 +0200

    Updates

---
+++ precache-manifest.2cbb4456bb342bcefef0353a81b71b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8177d0e1bbb95480de62af6e09f5d0",
+    "url": "/OpenTrafficLightsPredictionVisualizer/index.html"
+  },
+  {
+    "revision": "4a68dccbbc57e683fce9",
+    "url": "/OpenTrafficLightsPredictionVisualizer/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "edac865a8fd8159f28e3",
+    "url": "/OpenTrafficLightsPredictionVisualizer/static/css/main.e5e4e546.chunk.css"
+  },
+  {
+    "revision": "4a68dccbbc57e683fce9",
+    "url": "/OpenTrafficLightsPredictionVisualizer/static/js/2.e04ea0a4.chunk.js"
+  },
+  {
+    "revision": "edac865a8fd8159f28e3",
+    "url": "/OpenTrafficLightsPredictionVisualizer/static/js/main.1d03fdc7.chunk.js"
+  },
+  {
+    "revision": "ff4809defcf5de793dd8",
+    "url": "/OpenTrafficLightsPredictionVisualizer/static/js/runtime~main.f919e440.js"
+  }
+]);
